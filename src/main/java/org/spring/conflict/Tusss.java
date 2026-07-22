992e3c408cf4e2e7db3005dddc043b6497c46d82
Baitur <tbaitur@gmail.com>
2026-07-23 04:14:00 +0600

    good boooy

--- src/main/java/org/spring/conflict/Tusss.java
+++ src/main/java/org/spring/conflict/Tusss.java
@@ -2,6 +2,6 @@ package org.spring.conflict;
 
 public class Tusss {
     public static void main(String[] args) {
-        System.out.println("Wow wow yoo");
+        System.out.println("good boooy");
     }
 }
