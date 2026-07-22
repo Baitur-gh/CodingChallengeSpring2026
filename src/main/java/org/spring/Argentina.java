f7801a4a94802bca492aaf6ade8669917a207ee5
Baitur <tbaitur@gmail.com>
2026-07-23 04:24:52 +0600

    Spain is champ

--- src/main/java/org/spring/Argentina.java
+++ src/main/java/org/spring/Argentina.java
@@ -2,9 +2,6 @@ package org.spring;
 
 public class Argentina {
     public static void main(String[] args) {
-
-        System.out.println("Bye bye Argentina");             System.out.println("Argentina is not champions!!!");
         System.out.println("Argentina is not champions!!!");
-
     }
 }
